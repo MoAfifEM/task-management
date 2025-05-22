65e854667dbaba9831856a4cbe30ce4cf503a396
MoAfifEM <afif.ramzi@encoremed.io>
2025-05-22 09:40:29 +0800

    refactor: comment out unused staff routes in router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,30 +41,30 @@ const router = createRouter({
     //   component: () => import('../pages/journeys/Index.vue'),
     //   meta: { requiresAuth: true },
     // },
-    {
-      path: '/staff/tasks',
-      name: 'staffTasks',
-      component: () => import('../pages/staff/MyTask.vue'),
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/staff/plans',
-      name: 'staffPlans',
-      component: () => import('../pages/staff/TaskPlan.vue'),
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/staff/journeys',
-      name: 'staffJourneys',
-      component: () => import('../pages/staff/Journey.vue'),
-      meta: { requiresAuth: true },
-    },
-    {
-      path: '/staff/patients',
-      name: 'staffPatients',
-      component: () => import('../pages/patients/Index.vue'),
-      meta: { requiresAuth: true },
-    },
+    // {
+    //   path: '/staff/tasks',
+    //   name: 'staffTasks',
+    //   component: () => import('../pages/staff/MyTask.vue'),
+    //   meta: { requiresAuth: true },
+    // },
+    // {
+    //   path: '/staff/plans',
+    //   name: 'staffPlans',
+    //   component: () => import('../pages/staff/TaskPlan.vue'),
+    //   meta: { requiresAuth: true },
+    // },
+    // {
+    //   path: '/staff/journeys',
+    //   name: 'staffJourneys',
+    //   component: () => import('../pages/staff/Journey.vue'),
+    //   meta: { requiresAuth: true },
+    // },
+    // {
+    //   path: '/staff/patients',
+    //   name: 'staffPatients',
+    //   component: () => import('../pages/patients/Index.vue'),
+    //   meta: { requiresAuth: true },
+    // },
     // V2 style
     {
       path: '/v2/patients',
